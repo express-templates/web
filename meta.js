2cef8009fcc2120a375d1aeba20ec5f3c45cb172
tachibana-shin <thanhnguyennguyen1995@gmail.com>
2021-11-27 19:12:43 +0700

    update for new pkgs

--- meta.js
+++ meta.js
@@ -1,11 +1,6 @@
 const path = require("path");
 const fs = require("fs");
 
-const {
-  sortDependencies,
-  installDependencies,
-  printMessage,
-} = require("./utils");
 const pkg = require("./package.json");
 
 const templateVersion = pkg.version;
@@ -238,52 +233,11 @@ module.exports = {
       message: "Timeout query database?",
       default: 60000,
     },
-    autoInstall: {
-      type: "list",
-      message:
-        "Should we run `npm install` for you after the project has been created? (recommended)",
-      choices: [
-        {
-          name: "Yes, use Yarn",
-          value: "yarn",
-          short: "yarn",
-        },
-        {
-          name: "Yes, use NPM",
-          value: "npm",
-          short: "npm",
-        },
-        {
-          name: "No, I will handle that myself",
-          value: false,
-          short: "no",
-        },
-      ],
-    },
   },
   filters: {
     "src/boot/database.*": "database !== null",
     "tsconfig.json": "ts",
     "**/*.js": "ts === false",
     "**/*.ts": "ts",
-  },
-  complete: function (data, { chalk }) {
-    const green = chalk.green;
-
-    sortDependencies(data, green);
-
-    const cwd = path.join(process.cwd(), data.inPlace ? "" : data.destDirName);
-
-    if (data.autoInstall) {
-      installDependencies(cwd, data.autoInstall, green)
-        .then(() => {
-          printMessage(data, green);
-        })
-        .catch((e) => {
-          console.log(chalk.red("Error:"), e);
-        });
-    } else {
-      printMessage(data, chalk);
-    }
   },
 };
